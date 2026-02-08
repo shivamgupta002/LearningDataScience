346dfd5e36ed024bc73cbea3b5e0398101734b2c
shivamgupta002 <shivamgupta992909@gmail.com>
2026-02-08 14:24:19 +0530

    Python part-1

---
+++ Python/Part-1/Solution-1.py
@@ -0,0 +1,87 @@
+#### Q1 ####
+
+# name=input("Please enter you name : ")
+# age=int(input("Please enter age : "))
+
+# print("Hello",name,", you are", age ,"years old !")
+
+
+#### Q2 ####
+
+# a = float(input("Enter 1st number :"))
+# b = float(input("Enter 2nd number :"))
+
+# print("Sum :",a+b)
+# print("Difference :",a-b)
+# print("Product :",a*b)
+# print("Quotient :",a/b)
+
+
+#### Q3 ####
+
+# a = int(input("Enter 1st number :"))
+# b = int(input("Enter 2nd number :"))
+# b = float(input("Enter 2nd number :"))
+
+# avg=float((a+b+b)/3)
+
+# print("Average of 3 numbers :",avg)
+
+
+#### Q4 ####
+
+# a = input("Enter a number :")
+
+# print(int(a), type(int(a)))
+# print(float(a), type(float(a)))
+# print(str(a), type(str(a)))
+
+#### Q5 ####
+
+# x= 10 + 3 * 2 ** 2
+# print(x)
+
+# '''  Solution->
+#     10+3*4
+#     10+12
+#     22 
+# '''
+
+
+#### Q6 ####
+
+# a = int(input("Enter 1st number :"))
+# b = int(input("Enter 2nd number :"))
+
+# temp=a
+# a=b
+# b=temp
+
+# print("a = ", a)
+# print("b = ", b)
+
+#### Q7 ####
+
+# celcius = float(input("Enter temp in celsius :"))
+
+# ft=(celcius*(9/5))+32
+
+# print("Temperature in FahrenheitTemp is : ", ft)
+
+
+#### Q8 ####
+
+# radius = float(input("Enter radius of circle :"))
+# area=3.14*(radius**2)
+# print("Area of circle : ",area)
+
+
+#### Q9 ####
+
+P = float(input("Enter Principal (P) :"))
+R = float(input("Enter Rate (R) :"))
+T = float(input("Enter Time (T) :"))
+
+SI=(P*R*T)/100
+
+print("Simple interest is ",SI)
